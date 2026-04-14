93f6bbcffc1782f73192ede33c60a3ce5597203f
dianagrebennikova <dianagrebennikova97@mail.ru>
2026-04-14 20:00:48 +0300

    add imports

--- src/modules/users.js
+++ src/modules/users.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path')
+const fs = require('node:fs');
+const path = require('node: path')
 
 const getUsers = () => {
     const filePath = path.join(__dirname, '../data/users.json')
